f31801b3e4a92e07e7896e25b1e65794e73d0159
mehmetdervis <mhmtdrvss@gmail.com>
2025-07-14 08:32:11 +0300

    Add files via upload

---
+++ grade_average_calculator.py
@@ -0,0 +1,6 @@
+birinci_not=float(input("Birinci notu giriniz :"))
+ikinci_not=float(input("İkinci notu giriniz :"))
+ucuncu_not=float(input("Üçüncü notu giriniz :"))
+Ortalama = (birinci_not + ikinci_not + ucuncu_not)/ 3
+print(round(Ortalama,1))
+
